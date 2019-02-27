b380f912939293decfbc7297259919ee2d9d1cc6
comprobot <comprobot0503@gmail.com>
2019-02-27 15:58:43 +0800

    Update check.php

--- check.php
+++ check.php
@@ -1,18 +1,27 @@
 <?php
 session_start();
-$db = mysqli_connect('localhost', 'ops', '60503176', 'ops');
+$servername = 'localhost';
+$username = 'ops';
+$password = '60503176';
+$dbname = 'ops';
+// Create connection
 if(isset($_GET['username'])){
-                $username = mysqli_real_escape_string($db, $_POST['username']);
-				        if (empty($username)) { 
-					        echo 'empty';
-				        }
-				
-                $sql = $db->query("SELECT * FROM users WHERE username  = '$username'");
-                if($sql->rowCount() > 0){
-                     echo 'user exist, please choose another';
-                }else{
-                    echo 'available';
-                }
+$username1 = mysqli_real_escape_string($db, $_POST['username']);
 
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
 }
-?php>
+$sql = "SELECT * FROM `adb_users` WHERE = '$username1'";
+$result = $conn->query($sql);
+if ($result->num_rows > 0) {
+    // output data of each row
+    echo " 1 results"
+} else {
+    echo "0 results";
+}
+$conn->close();
+}
+?>
