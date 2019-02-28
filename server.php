8e882b2b648fa35e2a037b7cf0f24e2d44acb353
comprobot <comprobot0503@gmail.com>
2019-02-28 17:09:25 +0800

    Update server.php

--- server.php
+++ server.php
@@ -58,7 +58,7 @@ if (isset($_POST['register'])) {
   
   
   
-  array_push($errors, "I dont know ");
+  
   
   
   
@@ -69,44 +69,29 @@ if (isset($_POST['register'])) {
   $result = mysqli_query($db, $user_check_query);
   $user = mysqli_fetch_assoc($result);
   
-  
-  array_push($errors, "I dont know sql");
-  
   if ($user) { // if user exists
-    if ($user['username'] == $username) {
+    if ($user['username'] === $username) {
       array_push($errors, "Username already exists");
     }
 
-    if ($user['email'] == $email) {
+    if ($user['email'] === $email) {
       array_push($errors, "email already exists");
     }
   }
 
-  
-  // Finally, register user if there are no errors in the form
   /*
+  // Finally, register user if there are no errors in the form
   if (count($errors) == 0) {
-  	//$password = md5($password_1);//encrypt the password before saving in the database
-	
-	array_push($errors, "before sql \n");
+  	$password = md5($password_1);//encrypt the password before saving in the database
 
-  	$query = "INSERT INTO adv_users (username, email, password,first_name, last_name,area_code, phone, title) 
-  			  VALUES('$username', '$email', '$password', '$first_name', '$last_name', '$area_code', '$phone', '$title')";
+  	$query = "INSERT INTO users (username, email, password) 
+  			  VALUES('$username', '$email', '$password')";
   	mysqli_query($db, $query);
-	
-	array_push($errors, "after sql \n");
-	
-	
   	$_SESSION['username'] = $username;
   	$_SESSION['success'] = "You are now logged in";
-	
-	array_push($errors, "3 login in");
-	
-	
-  	//header('location: home.php');
+  	header('location: index.php');
   }
   */
-  
 }
 
 // ... 
