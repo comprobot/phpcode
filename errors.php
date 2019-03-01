0f83a94cd3b1c5709ba67678cdd85ecb1c8a651f
comprobot <comprobot0503@gmail.com>
2019-03-01 14:44:49 +0800

    Update errors.php

--- errors.php
+++ errors.php
@@ -1,8 +1,3 @@
-<?php
-   $username = mysqli_real_escape_string($db, $_POST['username']);
-   echo $username
-?>
-
 <?php  if (count($errors) > 0) : ?>  
   	<?php foreach ($errors as $error) : ?>
   	  <p><?php echo $error ?></p>
