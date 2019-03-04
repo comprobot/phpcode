44b4c62a414abd5185399862312a77d7c66c048f
comprobot <comprobot0503@gmail.com>
2019-03-04 10:50:02 +0800

    Update videoquery.php

--- videoquery.php
+++ videoquery.php
@@ -1,15 +1,16 @@
 <?php
 $servername = 'localhost';
-$username = 'ops'
-$passwordc = '60503176';
+$dbusername = 'ops';
+$dbpassword = '60503176';
 $dbname = 'ops';
 // Create connection
-$conn = new mysqli($servername, $username, $passwordc, $dbname);
+$db = new mysqli($servername, $dbusername, $dbpassword, $dbname);
+
 // Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+if ($db->connect_error) {
+    die("Connection failed: " . $db->connect_error);
 }
-$sql = "SELECT filename, qrcode FROM advs_video";
+$db = "SELECT filename, qrcode FROM advs_video";
 $result = $conn->query($sql);
 if ($result->num_rows > 0) {
     // output data of each row
