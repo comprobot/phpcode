72d715ee0ad760794139f4662cf21b0fef9f801c
comprobot <comprobot0503@gmail.com>
2019-03-04 10:31:39 +0800

    Create videoquery.php

---
+++ videoquery.php
@@ -0,0 +1,23 @@
+<?php
+$servername = 'localhost';
+$username = 'ops'
+$password = '60503176';
+$dbname = 'ops';
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+$sql = "SELECT filename, qrcode FROM `advs_video`";
+$result = $conn->query($sql);
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo $row["filename"].",". $row["qrcode"]."\n";
+    }
+} else {
+    echo "0 results";
+}
+$conn->close();
+?>
