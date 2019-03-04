94b44518e4a3d1f10c3a375ee4105b4379801138
comprobot <comprobot0503@gmail.com>
2019-03-04 10:43:06 +0800

    Update videoquery.php

--- videoquery.php
+++ videoquery.php
@@ -9,12 +9,12 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-$sql = "SELECT filename, qrcode FROM `advs_video`";
+$sql = "SELECT filename, qrcode FROM advs_video";
 $result = $conn->query($sql);
 if ($result->num_rows > 0) {
     // output data of each row
     while($row = $result->fetch_assoc()) {
-        echo $row["filename"].",". $row["qrcode"]."\n";
+        echo $row["filename"].','. $row["qrcode"].'\n';
     }
 } else {
     echo "0 results";
