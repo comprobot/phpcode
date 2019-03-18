fb0cacc3a4bc20701ab47e0ac21e7ea63a7b5f7c
comprobot <comprobot0503@gmail.com>
2019-03-18 15:00:00 +0800

    Update server.php

--- server.php
+++ server.php
@@ -13,6 +13,9 @@ $cpassword="";
 $title="";
 $industry="";
 $package="";
+$store_name="";
+$store_address="";
+$target_user="";
 $servername = 'localhost';
 $dbusername = 'ops';
 $dbpassword = '60503176';
@@ -133,7 +136,7 @@ if (isset($_POST['upload_video'])) {
 				$query2 = "SELECT * FROM advs_video WHERE username='$username'";
 				$results = mysqli_query($db, $query2);
 				if (mysqli_num_rows($results) == 1) {
-					$query = "UPDATE advs_video SET filename= '$filename' , qrcode ='$qrcode_str' WHERE username='$username'";
+					$query = "UPDATE advs_video SET filename= '$filename' , qrcode ='$qrcode_str', approved ='P' WHERE username='$username'";
 					if ($db->query($query) === TRUE) {
  				        
 					} else {
@@ -141,7 +144,7 @@ if (isset($_POST['upload_video'])) {
 					}
 					
 				}else {
-					$query = "INSERT INTO advs_video (username, filename , qrcode )  VALUES('$username', '$filename', '$qrcode_str')";
+					$query = "INSERT INTO advs_video (username, filename , qrcode, approved )  VALUES('$username', '$filename', '$qrcode_str','P')";
 					if ($db->query($query) === TRUE) {
  				        
 					} else {
@@ -182,6 +185,101 @@ if (isset($_POST['upload_video'])) {
 	
 	
 }
+
+
+
+
+
+
+// REGISTER USER
+if (isset($_POST['register_store'])) {
+	
+	
+  // receive all input values from the form
+  $username = mysqli_real_escape_string($db, $_POST['username']);
+  $email = mysqli_real_escape_string($db, $_POST['email']);
+  $password = mysqli_real_escape_string($db, $_POST['password']);
+  $cpassword = mysqli_real_escape_string($db, $_POST['cpassword']);  
+  $first_name = mysqli_real_escape_string($db, $_POST['first_name']);
+  $last_name = mysqli_real_escape_string($db, $_POST['last_name']);
+  $area_code = mysqli_real_escape_string($db, $_POST['area_code']);
+  $phone = mysqli_real_escape_string($db, $_POST['phone']);
+  $industry = mysqli_real_escape_string($db, $_POST['industry']);
+  
+  $store_name = mysqli_real_escape_string($db, $_POST['store_name']);
+  $store_address = mysqli_real_escape_string($db, $_POST['store_address']);
+  $target_user = mysqli_real_escape_string($db, $_POST['target_user']);
+  
+  
+  $title = mysqli_real_escape_string($db, $_POST['title']);
+  
+  /*
+  
+  $store_name="";
+$store_address="";
+$target_user="";
+
+  
+  */
+  
+  
+
+  // form validation: ensure that the form is correctly filled ...
+  // by adding (array_push()) corresponding error unto $errors array
+  if (empty($username)) { array_push($errors, "Username is required"); }
+  if (empty($email)) { array_push($errors, "Email is required"); }
+  if (empty($first_name)) { array_push($errors, "First name is required"); }
+  if (empty($last_name)) { array_push($errors, "Last name is required"); }
+  if (empty($area_code)) { array_push($errors, "Area code is required"); }
+  if (empty($phone)) { array_push($errors, "Phone is required"); }
+  if (empty($title)) { array_push($errors, "Title is required"); }
+  if (empty($package)) { array_push($errors, "Title is required"); }
+  if (empty($password)) { array_push($errors, "Password is required"); }  
+  if (empty($industry)) { array_push($errors, "Industry is required"); }  
+  if (empty($store_name)) { array_push($errors, "Store name is required"); }  
+  if (empty($store_address)) { array_push($errors, "Store address is required"); }  
+  if (empty($target_user)) { array_push($errors, "Target user is required"); }  
+  
+  
+  if (empty($cpassword)) { array_push($errors, "Confirm password is required"); }  
+  if ($password != $cpassword) {
+	array_push($errors, "The two passwords do not match");
+  }
+  
+  //header('location: check.php?username=ronald');
+  // first check the database to make sure 
+  // a user does not already exist with the same username and/or email
+  $user_check_query = "SELECT * FROM store_users WHERE username='$username' LIMIT 1";
+  $result = mysqli_query($db, $user_check_query);
+  $user = mysqli_fetch_assoc($result);
+  
+  if ($user) { // if user exists
+    if ($user['username'] === $username) {
+      array_push($errors, "Username already exists");
+    }
+
+    if ($user['email'] === $email) {
+      array_push($errors, "email already exists");
+    }
+  }
+
+  
+  // Finally, register user if there are no errors in the form
+  if (count($errors) == 0) {
+  	//$password = md5($password_1);//encrypt the password before saving in the database
+
+  	$query = "INSERT INTO store_users (username, email, password,first_name, last_name,area_code, phone, title,industry, store_name, store_address, target_user) 
+  			  VALUES('$username', '$email', '$password', '$first_name', '$last_name', '$area_code', '$phone', '$title', '$industry', '$store_name', '$store_address', '$target_user')";
+  	mysqli_query($db, $query);
+  	$_SESSION['username'] = $username;
+  	$_SESSION['success'] = "You are now logged in";
+  	header('location: storehome.php');
+  }
+  
+}
+
+
+
 
 
 if (isset($_GET['approvel'])) {
