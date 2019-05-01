c0cc4bb2cc802199d2012bb7c20aa3964f7ed471
comprobot <comprobot0503@gmail.com>
2019-05-01 15:51:14 +0800

    Update server.php

--- server.php
+++ server.php
@@ -108,7 +108,6 @@ if (isset($_POST['register'])) {
 }
 
 
-
 if (isset($_POST['add_media_player'])) {
 	
 	$serial_number = mysqli_real_escape_string($db, $_POST['serial_number']);
@@ -128,6 +127,8 @@ if (isset($_POST['add_media_player'])) {
 	}
 	
 }
+
+
 
 
 if (isset($_GET['delete_media_player'])) {
@@ -404,8 +405,85 @@ if (isset($_GET['approvel'])) {
 
 
 
+if (isset($_POST['register_customer'])) {
+  // receive all input values from the form
+  $username = mysqli_real_escape_string($db, $_POST['username']);  
+  $password = mysqli_real_escape_string($db, $_POST['password']);
+  $cpassword = mysqli_real_escape_string($db, $_POST['cpassword']);    
+  $area_code = mysqli_real_escape_string($db, $_POST['area_code']);
+  $phone = mysqli_real_escape_string($db, $_POST['phone']);
+  
+  
+  
+
+  // form validation: ensure that the form is correctly filled ...
+  // by adding (array_push()) corresponding error unto $errors array
+  if (empty($username)) { array_push($errors, "Username is required"); }
+  if (empty($area_code)) { array_push($errors, "Area code is required"); }
+  if (empty($phone)) { array_push($errors, "Phone is required"); }
+  if (empty($password)) { array_push($errors, "Password is required"); }  
+  if (empty($cpassword)) { array_push($errors, "Confirm password is required"); }  
+  if ($password != $cpassword) {
+	array_push($errors, "The two passwords do not match");
+  }
+  
+  $user_check_query = "SELECT * FROM customers WHERE username='$username' LIMIT 1";
+  $result = mysqli_query($db, $user_check_query);
+  $user = mysqli_fetch_assoc($result);
+  
+  if ($user) { // if user exists
+    if ($user['username'] === $username) {
+      array_push($errors, "Username already exists");
+    }
+  }
+
+  
+  // Finally, register user if there are no errors in the form
+  if (count($errors) == 0) {
+  	//$password = md5($password_1);//encrypt the password before saving in the database
+
+  	$query = "INSERT INTO customers (username, password,area_code, phone, point) 
+  			  VALUES('$username','$password', '$area_code', '$phone', 0 )";
+  	mysqli_query($db, $query);
+  	$_SESSION['username'] = $username;
+  	$_SESSION['success'] = "You are now logged in";
+  	header('location: customerhome.php');
+  }
+  
+}
 
 
+
+
+
+
+
+
+
+if (isset($_POST['customer_login_user'])) {
+  $username = mysqli_real_escape_string($db, $_POST['username']);
+  $password = mysqli_real_escape_string($db, $_POST['password']);
+
+  if (empty($username)) {
+  	array_push($errors, "Username is required");
+  }
+  if (empty($password)) {
+  	array_push($errors, "Password is required");
+  }
+
+  if (count($errors) == 0) {
+  	//$password = md5($password);
+  	$query = "SELECT * FROM customers WHERE username='$username' AND password='$password'";
+  	$results = mysqli_query($db, $query);
+  	if (mysqli_num_rows($results) == 1) {
+  	  $_SESSION['username'] = $username;
+  	  $_SESSION['success'] = "You are now logged in";
+  	  header('location: customerhome.php');
+  	}else {
+  		array_push($errors, "Wrong username/password combination");
+  	}
+  }
+}
 
 
 
