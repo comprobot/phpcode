605d7ff86bc05e51e8583dc5bac28755e6a253ac
comprobot <comprobot0503@gmail.com>
2019-05-02 15:54:03 +0800

    Update restap.php

--- restap.php
+++ restap.php
@@ -96,4 +96,61 @@ if (isset($_GET['register_customer'])) {
 }
 
 
+if (isset($_GET['customer_earn_point'])) {
+  $username = mysqli_real_escape_string($db, $_GET['username']);
+  $password = mysqli_real_escape_string($db, $_GET['password']);
+
+  if (empty($username)) {
+  	array_push($errors, "Username is required");
+  }
+  if (empty($password)) {
+  	array_push($errors, "Password is required");
+  }
+
+  if (count($errors) == 0) {
+  	//$password = md5($password);
+	
+	
+	
+  	$query = "SELECT POINT FROM customers WHERE username='$username' AND password='$password'";
+  	$results = mysqli_query($db, $query);
+  	if (mysqli_num_rows($results) == 1) {
+  	  
+	$earnpoint = "UPDATE customers  SET point = point + 1  WHERE username='$username' AND password='$password'";  
+	if ($db->query($earnpoint) === TRUE) {		
+	   
+ 		echo "<p>SUCCESS</p>";        
+	} else {
+		
+	    
+	}	  
+	  
+  	}else {
+  		array_push($errors, "Wrong username/password combination");
+  	}
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 ?>
