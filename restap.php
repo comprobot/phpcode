c5b3704af66fd625e2048605c2838ff7ced963ca
comprobot <comprobot0503@gmail.com>
2019-05-04 15:19:47 +0800

    Update restap.php

--- restap.php
+++ restap.php
@@ -98,7 +98,7 @@ if (isset($_GET['register_customer'])) {
 
 if (isset($_GET['qrcode_customer'])) {
   $customer_username = mysqli_real_escape_string($db, $_GET['customer_name']);
-  $password = mysqli_real_escape_string($db, $_GET['password']);
+  $password1 = mysqli_real_escape_string($db, $_GET['password']);
   $store_username = mysqli_real_escape_string($db, $_GET['store_username']);
   $serial_number = mysqli_real_escape_string($db, $_GET['serial_number']);
   $adv_user = mysqli_real_escape_string($db, $_GET['adv_user']);
@@ -109,7 +109,7 @@ if (isset($_GET['qrcode_customer'])) {
   if (empty($customer_username)) {
   	array_push($errors, "Username is required");
   }
-  if (empty($password)) {
+  if (empty($password1)) {
   	array_push($errors, "Password is required");
   }
 
@@ -127,9 +127,9 @@ if (isset($_GET['qrcode_customer'])) {
   if (count($errors) == 0) {
   	//$password = md5($password);
 	
+	//http://157.230.145.40/ops/restap.php?qrcode_customer=qrcode_customer&store_username=storeusr&serial_number=sr-156&adv_user=linux&customer_name=ronald&cpassword=60503176
 	
-	
-  	$query = "SELECT POINT FROM customers WHERE username='$username' AND password='$password'";
+  	$query = "SELECT POINT FROM customers WHERE username='$username' AND password='$password1'";
   	$results = mysqli_query($db, $query);
   	if (mysqli_num_rows($results) == 1) {
   	  
@@ -168,27 +168,5 @@ if (isset($_GET['qrcode_customer'])) {
   	}
   }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 ?>
