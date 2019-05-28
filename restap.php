6a629ec77ba3fdaad7c14c01754401960546f9dc
comprobot <comprobot0503@gmail.com>
2019-05-28 14:59:00 +0800

    Update restap.php

--- restap.php
+++ restap.php
@@ -19,6 +19,7 @@ $target_user="";
 $serial_number="";
 $info="";
 $id="";
+$age="";
 
 $servername = 'localhost';
 $dbusername = 'ops';
@@ -33,6 +34,84 @@ $errors = array();
 //$db = mysqli_connect('localhost', 'root', '', 'registration');
 
 $db = new mysqli($servername, $dbusername, $dbpassword, $dbname);
+
+
+if (isset($_GET['reg_customer'])) {
+  // receive all input values from the form
+  $username = mysqli_real_escape_string($db, $_GET['username']);  
+  $password = mysqli_real_escape_string($db, $_GET['password']);
+  $cpassword = mysqli_real_escape_string($db, $_GET['cpassword']);    
+  $email = mysqli_real_escape_string($db, $_GET['email']);    
+  $age = mysqli_real_escape_string($db, $_GET['age']);   
+  $title = mysqli_real_escape_string($db, $_GET['title']);   
+  
+
+  // form validation: ensure that the form is correctly filled ...
+  // by adding (array_push()) corresponding error unto $errors array
+  if (empty($username)) { array_push($errors, "Username is required"); }
+  if (empty($age)) { array_push($errors, "Username is required"); }
+  if (empty($email)) { array_push($errors, "Email is required"); }
+  
+
+  
+  
+  if (empty($password)) { array_push($errors, "Password is required"); }  
+  if (empty($cpassword)) { array_push($errors, "Confirm password is required"); }  
+  if ($password != $cpassword) {
+	array_push($errors, "The two passwords do not match");
+  }
+  
+  
+  
+  
+  
+  $user_check_query = "SELECT * FROM customers WHERE username='$username' LIMIT 1";
+  $result = mysqli_query($db, $user_check_query);
+  $user = mysqli_fetch_assoc($result);
+  
+  if ($user) { // if user exists
+    if ($user['username'] === $username) {
+      array_push($errors, "Username already exists");
+    }
+  }
+
+  
+  // Finally, register user if there are no errors in the form
+  if (count($errors) == 0) {
+  	//$password = md5($password_1);//encrypt the password before saving in the database
+
+  	$query = "INSERT INTO customers (username, password,area_code, telephone, point, email, title, age) 
+  			  VALUES('$username','$password', '$area_code', '$phone', 0 ,'$email','$title','$age' )";
+  	//mysqli_query($db, $query);
+	
+	if ($db->query($query) === TRUE) {
+		echo "<p>SUCCESS</p>";
+ 		        
+	} else {
+		
+	       array_push($errors, "Error: " . $query . "<br>" . $db->error);    					
+		   
+		   
+		   
+	}
+	
+  }else{	  
+	  
+	  foreach ($errors as $error) {
+		echo "<p>".$error ."</p>";  
+	  } 
+  	    
+  }
+
+
+
+
+
+
+}
+
+
+
 if (isset($_GET['register_customer'])) {
   // receive all input values from the form
   $username = mysqli_real_escape_string($db, $_GET['username']);  
