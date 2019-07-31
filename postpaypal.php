efd6526e6f4dd5c354fbc74073e8a995d454cbc7
comprobot <comprobot0503@gmail.com>
2019-07-31 11:39:18 +0800

    Update postpaypal.php

--- postpaypal.php
+++ postpaypal.php
@@ -1,9 +1,32 @@
 <?php
-$host = "https://www.paypal.com";
+
+function send_post($url, $post_data) {
+ 
+    $postdata = http_build_query($post_data);
+    $options = array(
+        'http' => array(
+            'method' => 'POST',
+            'header' => 'Content-type:application/x-www-form-urlencoded',
+            'content' => $postdata,
+            'timeout' => 15 * 60 // 超时时间（单位:s）
+        )
+    );
+    $context = stream_context_create($options);
+    $result = file_get_contents($url, false, $context);
+ 
+    return $result;
+}
+
+
+
+
+$host = "https://www.paypal.com/cgi-bin/webscr";
 $path = "/cgi-bin/webscr";
 $data =  $_POST;;
 //$data = urlencode($data);
 
+
+send_post(%host, $data);
 
 
 $product_list = filter_input(INPUT_POST, 'cart_list');
@@ -35,7 +58,7 @@ if($product_list_array) {
 
 
 
-
+/*
 header("POST $path HTTP/1.1\\r\
 " );
 header("Host: $host\\r\
@@ -48,4 +71,6 @@ header("Connection: close\\r\
 \\r\
 " );
 header($data);
+
+*/
 ?>
