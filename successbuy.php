51f14481b02d237276d882b521820e0c0c84257d
comprobot <comprobot0503@gmail.com>
2019-08-13 11:02:19 +0800

    Update successbuy.php

--- successbuy.php
+++ successbuy.php
@@ -24,6 +24,8 @@ $servername = 'localhost';
 $dbusername = 'ops';
 $dbpassword = '60503176';
 $dbname = 'ops';
+$price = '';
+$item = '';
 
 $errors = array(); 
 
@@ -37,10 +39,14 @@ if (isset($_GET['add_payment'])) {
 	
 
 	$username = mysqli_real_escape_string($db, $_GET['username'] );
+	$item = mysqli_real_escape_string($db, $_GET['item'] );
+	$price = mysqli_real_escape_string($db, $_GET['price'] );
 	
 	if (empty($username)) { array_push($errors, "Username is required"); }
+	if (empty($item)) { array_push($errors, "Item ID is required"); }
+	if (empty($price)) { array_push($errors, "Price is required"); }
 
-	$query = "INSERT INTO customer_payment (customerid, paid , verified)  VALUES('$username', 'T', 'F')";
+	$query = "INSERT INTO customer_payment (customerid, paid ,item,price verified)  VALUES('$username', '$item',price,'T', 'F')";
 	if ($db->query($query) === TRUE) {
 		
 		echo "<h1>The advertisement plan you have sccessfully purcharsed. </h1>";
