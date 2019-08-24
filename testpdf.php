720af89a96fa63c4fab363c264eec5b883182f3c
comprobot <comprobot0503@gmail.com>
2019-08-24 18:37:56 +0800

    Update testpdf.php

--- testpdf.php
+++ testpdf.php
@@ -5,5 +5,10 @@ $pdf = new FPDF();
 $pdf->AddPage();
 $pdf->SetFont('Arial','B',16);
 $pdf->Cell(40,10,'Hello World!');
+
+$image1 = "pic/output.jpg";
+
+$pdf->Image('pic/'.$image1,40,60,100,100);
+
 $pdf->Output();
 ?>
