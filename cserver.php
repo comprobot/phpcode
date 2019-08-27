309aa10eafe12c7054f4be2b818fcac9eaf20d7b
comprobot <comprobot0503@gmail.com>
2019-08-27 18:24:44 +0800

    Update cserver.php

--- cserver.php
+++ cserver.php
@@ -108,6 +108,83 @@ if (isset($_POST['register'])) {
 }
 
 
+if (isset($_GET['buyitem'])) {
+  $username = mysqli_real_escape_string($db, $_GET['userid']);
+  $itemid = mysqli_real_escape_string($db, $_GET['itemid']);
+
+  if (empty($username)) {
+  	header('location: user_login.php');
+  }
+  
+  if (empty($itemid)) {
+  	header('location: user_login.php');
+  }
+  
+  //NEXTVAL('itemSeq')
+  
+    //check exist of memember
+	
+    $queryM = "SELECT * FROM customers WHERE username='$username'";
+	
+  	$results = mysqli_query($db, $queryM);
+  	if (mysqli_num_rows($results) == 1) {
+  
+		$query = "SELECT * FROM item_shop WHERE item_id = '$item_id' and item_quantity > 0";
+	
+		$results = mysqli_query($db, $query);
+		if (mysqli_num_rows($results) == 1) {
+		
+			$query2 = "UPDATE item_shop SET item_quantity =  item_quantity - 1 WHERE item_id='$item_id'";		
+		
+			if ($db->query($query2) === TRUE) {
+			
+			   // check redeem code exist:
+				$chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+				$res = "";
+				for ($i = 0; $i < 10; $i++) {
+					$res .= $chars[mt_rand(0, strlen($chars)-1)];
+				}			   
+			   
+			    $queryRedeemCode = "SELECT * FROM customer_item WHERE item_redeem_code = '$res'";
+				
+				while ($db->query($queryRedeemCode) === TRUE) {
+					$chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+					$res = "";
+					for ($i = 0; $i < 10; $i++) {
+						$res .= $chars[mt_rand(0, strlen($chars)-1)];
+					}			   
+					$queryRedeemCode = "SELECT * FROM customer_item WHERE item_redeem_code = '$res'";
+				}
+				
+			  
+				$query3 = "INSERT INTO customer_item (userid, item_id ,item_redeem_code, item_status)  VALUES('$username','$item_id', '$res', 'B')";
+				
+				if ($db->query($query3) === TRUE) {
+					
+				    header('location: redemption_processing.php');	
+					
+				}else{
+					
+					
+				}
+				        
+			} else {
+			
+			}
+		
+		}else {
+		
+			array_push($errors, "The item is out of stock");
+		
+		}
+	
+	}else {
+  		array_push($errors, "User does not exist ");
+  	}
+  
+  
+}
+
 if (isset($_POST['add_media_player'])) {
 	
 	$serial_number = mysqli_real_escape_string($db, $_POST['serial_number']);
