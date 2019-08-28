ec64aefbeb28d6fdca5e8538dc1cebcf7f41a055
comprobot <comprobot0503@gmail.com>
2019-08-28 15:28:09 +0800

    Update genEcoupon.php

--- genEcoupon.php
+++ genEcoupon.php
@@ -2,10 +2,16 @@
 require('fpdf.php');
 include "phpqrcode.php";    
 
-$target_dir = dirname(__FILE__) ."/pic/";
-$target_file = $target_dir . "temp.png";
+if (isset($_GET['redeem_code'])) {
+	
+  if (empty($_GET['redeem_code'])) {
+  	header('location: user_login.php');
+  }
 
-$value = 'http://157.230.145.40/ops/adv_user_home.php'; //二维码内容   
+$target_dir = dirname(__FILE__) ."/pic/";
+$target_file = $target_dir . "tempcode.png";
+
+$value = 'http://157.230.145.40/ops/redeemption_claim.php?redeem_code='.$_GET['redeem_code']; //二维码内容   
 $errorCorrectionLevel = 'L';//容错级别   
 $matrixPointSize = 6;//生成图片大小   
 //生成二维码图片   
@@ -18,12 +24,20 @@ $pdf->SetFont('Arial','B',16);
 $pdf->Cell(40,10,'E coupon redeemption code below for scanning');
 
 
-$image1 = "img/output.png";
+$pdf->SetFont('Arial','B',16);
+$pdf->Cell(40,30,'Redeem code='.$_GET['redeem_code']);
+
+
 $pdf->Image($target_file,40,40,100,100);
 
 /*
 $pdf->SetFont('Arial','B',16);
 $pdf->Cell(40,40,'Hello World!');
 */
+
+
 $pdf->Output();
+
+}
+
 ?>
