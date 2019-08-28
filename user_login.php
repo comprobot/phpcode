06b772fe8fd5043df113b09b3900c5de66bed1e1
comprobot <comprobot0503@gmail.com>
2019-08-28 10:48:51 +0800

    Update user_login.php

--- user_login.php
+++ user_login.php
@@ -5,7 +5,7 @@
         <title>Login Page</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="http://www.fivegold.hk/qrjetso_ui/style.css"/>
+        <link rel="stylesheet" href="style.css"/>
         <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
         <link href="https://fonts.googleapis.com/css?family=Noto+Sans+TC&display=swap" rel="stylesheet">        
     </head>
@@ -13,18 +13,18 @@
         <div id="header">
             <div id="logo_bar">
                 <div id="back">
-                    <a href="home.html"><img src="http://www.fivegold.hk/qrjetso_ui/img/back.png" srcset="http://www.fivegold.hk/qrjetso_ui/img/back-2x.png 2x, http://www.fivegold.hk/qrjetso_ui/img/back-3x.png 3x, http://www.fivegold.hk/qrjetso_ui/img/back-4x.png 4x"/></a>
+                    <a href="home.html"><img src="img/back.png" srcset="img/back-2x.png 2x, img/back-3x.png 3x, img/back-4x.png 4x"/></a>
                 </div>
                 <div id="logo">
-                    <a href="home.html"><img src="http://www.fivegold.hk/qrjetso_ui/img/logo.png" srcset="http://www.fivegold.hk/qrjetso_ui/img/logo-2x.png 2x, http://www.fivegold.hk/qrjetso_ui/img/logo-3x.png 3x, http://www.fivegold.hk/qrjetso_ui/img/logo-4x.png 4x"/></a>
+                    <a href="home.html"><img src="img/logo.png" srcset="img/logo-2x.png 2x, img/logo-3x.png 3x, img/logo-4x.png 4x"/></a>
                 </div>
                 <div id="next">
                 </div>
             </div>
             <div class="sperator"></div>
-        </div>
-	
-	    <div id="page">
+        </div>	    
+	    
+            <div id="page">
                 <div data-role="main" class="ui-content">
                     <form method="post" action="user_login.php" data-ajax="false"  id="loginform" name="loginForm">
                         <label for="name">用戶名稱 : <span>*</span></label>
@@ -39,15 +39,12 @@
 					
                 </div>
                 
-        <div id="footer_container">
-            <div class="sperator"></div>
+      <div id="footer_container">
             <div id="bottom_menu">
-                <div><a href="home.html"><img src="http://www.fivegold.hk/qrjetso_ui/img/btn_home.png" /></a></div>
-                <div><a href="home.html"><img src="http://www.fivegold.hk/qrjetso_ui/img/btn_search.png" /></a></div>
-                <div><a href="registration_1.html"><img src="http://www.fivegold.hk/qrjetso_ui/img/btn_inbox.png" /></a></div>
-                <div><a href="account.html"><img src="http://www.fivegold.hk/qrjetso_ui/img/btn_account.png" /></a></div>
-                <div><a href="home.html"><img src="http://www.fivegold.hk/qrjetso_ui/img/btn_scan.png" /></a></div>
+                <div><a href="home.html"><img src="img/btn_home.png" /></a></div>
+                <div><a href="account.html"><img src="img/btn_account.png" /></a></div>
+                <div><a href="guide_1.html"><img src="img/btn_scan.png" /></a></div>
             </div>
-        </div>		
+        </div>	
     </body>
 </html>
