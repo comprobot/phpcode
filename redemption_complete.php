43ec2213e4b39c81355f84f643f85f9464e8584c
comprobot <comprobot0503@gmail.com>
2019-08-28 17:12:59 +0800

    Add files via upload

---
+++ redemption_complete.php
@@ -0,0 +1,40 @@
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Redemption Claim</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="style.css"/>
+        <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+        <link href="https://fonts.googleapis.com/css?family=Noto+Sans+TC&display=swap" rel="stylesheet">        
+    </head>
+    <body>
+       <div id="header">
+            <div id="logo_bar">
+                <div id="back">
+                    <a href="home.html"><img src="img/back.png" srcset="img/back-2x.png 2x, img/back-3x.png 3x, img/back-4x.png 4x"/></a>
+                </div>
+                <div id="logo">
+                    <a href="home.html"><img src="img/logo.png" srcset="img/logo-2x.png 2x, img/logo-3x.png 3x, img/logo-4x.png 4x"/></a>
+                </div>
+                <div id="next">
+                </div>
+            </div>
+            <div class="sperator"></div>
+        </div>
+        <div id="page">		
+		    <div id="points">你的禮物已能成功兌換</div>				
+			<div id="points">回到<a href="redemption_user.php">換領禮物</a></div>				
+			<div id="points">回到<a href="redemption_processing.php">待換領記錄</a></div>				
+		    <div id="points">回到<a href="redemption_processing.php">已換領記錄</a></div>				            
+        </div>
+       <div id="footer_container">
+            <div id="bottom_menu">
+                <div><a href="home.html"><img src="img/btn_home.png" /></a></div>
+                <div><a href="account.html"><img src="img/btn_account.png" /></a></div>
+                <div><a href="guide_1.html"><img src="img/btn_scan.png" /></a></div>
+            </div>
+        </div>
+    </body>
+</html>
