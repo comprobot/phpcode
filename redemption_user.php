c792bea0053c9b15823f916020d409e9a9104e8a
comprobot <comprobot0503@gmail.com>
2019-08-28 10:04:05 +0800

    Update redemption_user.php

--- redemption_user.php
+++ redemption_user.php
@@ -17,7 +17,7 @@
         <title>Redemption List</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="http://www.fivegold.hk/qrjetso_ui/style.css"/>
+        <link rel="stylesheet" href="style.css"/>
         <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
         <link href="https://fonts.googleapis.com/css?family=Noto+Sans+TC&display=swap" rel="stylesheet">        
     </head>
