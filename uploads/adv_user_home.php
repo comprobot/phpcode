fb1480fbf11e305138ca0a167ffbcc047f235670
comprobot <comprobot0503@gmail.com>
2019-09-02 12:04:59 +0800

    Add files via upload

---
+++ uploads/adv_user_home.php
@@ -0,0 +1,622 @@
+<?php 
+  session_start(); 
+
+  if (!isset($_SESSION['username'])) {
+  	$_SESSION['msg'] = "You must log in first";
+  	header('location: adv_user_login.php');
+  }
+  if (isset($_GET['logout'])) {
+  	session_destroy();
+  	unset($_SESSION['username']);
+  	header("location: adv_user_login.php");
+  }
+?>
+<?php include('aserver.php') ?>
+
+<html>
+<!DOCTYPE html>
+<head>
+    <!-- Required meta tags-->
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="Colorlib Templates">
+    <meta name="author" content="Colorlib">
+    <meta name="keywords" content="Colorlib Templates">
+
+    <!-- Title Page-->
+    <title>廣告商網站</title>
+
+    <!-- Icons font CSS-->
+    <link href="vendor/mdi-font/css/material-design-iconic-font.min.css" rel="stylesheet" media="all">
+    <link href="vendor/font-awesome-4.7/css/font-awesome.min.css" rel="stylesheet" media="all">
+    <!-- Font special for pages-->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i" rel="stylesheet">
+
+    <!-- Vendor CSS-->
+    <link href="vendor/select2/select2.min.css" rel="stylesheet" media="all">
+    <link href="vendor/datepicker/daterangepicker.css" rel="stylesheet" media="all">
+
+    <!-- Main CSS-->
+    <link href="css/main.css" rel="stylesheet" media="all">
+    <!--<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>	-->
+	<script src="js/jquery.js"></script>
+	<script src="js/jquery.validate.js"></script>
+	<script src="js/additional-methods.js"></script>
+	<script src="js/messages_zh_TW.js"></script>
+	<link rel="stylesheet" href="src/style.css">
+    <link rel="stylesheet" href="src/jquery.horizontalmenu.css">
+    <script src="src/jquery.horizontalmenu.js"></script>
+	
+	
+	<script>
+	$.validator.setDefaults({
+		submitHandler: function() {
+			alert("submitted!");
+		}
+	});
+	
+	
+	</script>
+	
+	<!--<script src="js/registration.js"></script>	-->
+<script>
+
+		
+	(function ($) {
+  $.UrlParam = function (name) {
+    //宣告正規表達式
+    var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
+    /*
+     * window.location.search 獲取URL ?之後的參數(包含問號)
+     * substr(1) 獲取第一個字以後的字串(就是去除掉?號)
+     * match(reg) 用正規表達式檢查是否符合要查詢的參數
+    */
+    var r = window.location.search.substr(1).match(reg);
+    //如果取出的參數存在則取出參數的值否則回穿null
+    if (r != null) return unescape(r[2]); return null;
+  }
+})(jQuery);
+
+$(function () {
+  
+  $('#tab_list').horizontalmenu({
+                                itemClick: function (item) {
+                                $('#tab_list_content .ah-tab-content').removeAttr('data-ah-tab-active');
+                                
+                                
+                                $('#tab_list_content .ah-tab-content:eq(' + $(item).index() + ')').attr('data-ah-tab-active', 'true');
+                                
+                                
+                                
+                                return false; //if this finction return true then will be executed http request
+                                }
+                                });
+  
+  
+  //$('#sp1').attr('data-ah-tab-active', 'true');
+  
+  var param = $.UrlParam("tag");
+if (param=="customeraccess")
+{
+	$('#tab_list_content .ah-tab-content').removeAttr('data-ah-tab-active');	 
+	$('#customeraccess').attr('data-ah-tab-active', 'true');
+}
+if (param=="customerpayment")
+{
+	$('#tab_list_content .ah-tab-content').removeAttr('data-ah-tab-active');	 
+	$('#customerpayment').attr('data-ah-tab-active', 'true');
+	
+}
+if (param=="advvideo")
+{
+	$('#tab_list_content .ah-tab-content').removeAttr('data-ah-tab-active');	 
+	$('#advvideo').attr('data-ah-tab-active', 'true');
+	
+}
+
+if (param=="paymenthistory")
+{
+	$('#tab_list_content .ah-tab-content').removeAttr('data-ah-tab-active');	 
+	$('#paymenthistory').attr('data-ah-tab-active', 'true');
+	
+}
+
+
+if (param=="itemshop")
+{
+	$('#tab_list_content .ah-tab-content').removeAttr('data-ah-tab-active');	 
+	$('#itemshop').attr('data-ah-tab-active', 'true');
+	
+}
+
+if (param=="additem")
+{
+	$('#tab_list_content .ah-tab-content').removeAttr('data-ah-tab-active');	 
+	$('#itemshop').attr('data-ah-tab-active', 'true');
+	
+}
+
+
+//id="advvideo"
+	
+	
+  
+  
+  });
+
+
+$(document).ready(function() {
+	$('#uploadVideoForm').submit(function() {
+	
+	  //event.preventDefault();
+	  
+	  /*
+	
+	$("#uploadVideoForm").validate({
+	 rules: {				
+				qrcode_str: {
+					required: true,
+					minlength: 2
+				}
+			},
+			
+			
+			messages: {
+				qrcode_str: {
+					required: "Please enter a String for qr code",
+					minlength: "Your string for qr code must consist of at least 2 characters"
+				},
+			}
+		});
+	
+
+var qrcode_str = $("#qrcode_str").val();
+if (qrcode_str == '') {
+//alert("Please fill all fields...!!!!!!");
+} else {
+	
+}
+
+*/
+});
+
+
+
+
+$('#uploadItemForm').submit(function() {
+	
+	  //event.preventDefault();
+	
+	$("#uploadItemForm").validate({
+	 rules: {				
+				item_name: {
+					required: true,
+					minlength: 2
+				},
+				item_description: {
+					required: true,
+					minlength: 2
+				},
+				item_price: {
+					required: true,
+					minlength: 2
+				}
+				
+				
+			},
+			
+			
+			messages: {
+				item_str: {
+					required: "Please enter a String for Item name",
+					minlength: "Your string for item name must consist of at least 2 characters"
+				},
+				item_price: {
+					required: "Please enter a String for Item price",
+					minlength: "Your string for item price must consist of at least 2 characters"
+				},
+				item_description: {
+					required: "Please enter a String for Item description",
+					minlength: "Your string for item description must consist of at least 2 characters"
+				}
+			}
+		});
+	
+
+var item_name = $("#item_name").val();
+if (item_name == '') {
+//alert("Please fill all fields...!!!!!!");
+} else {
+	
+}
+
+var item_price = $("#item_price").val();
+if (item_name == '') {
+//alert("Please fill all fields...!!!!!!");
+} else {
+	
+}
+
+var item_name = $("#item_name").val();
+if (item_name == '') {
+//alert("Please fill all fields...!!!!!!");
+} else {
+	
+}
+
+}
+
+
+
+);
+
+
+
+
+
+
+
+
+});
+</script>	
+
+
+	
+</head>
+
+<body>
+    <div class="page-wrapper bg-gra-03 p-t-45 p-b-50">
+        <div class="wrapper wrapper--w790">
+            <div class="card card-5">
+                <div class="card-heading">
+                    <h2 class="title">廣告商網站</h2>
+                </div>
+                <div class="card-body">  
+
+           <div class="content">
+
+				
+            <?php  if (isset($_SESSION['username'])) : ?>
+			<div class="form-row">
+			     <div class="name">歡迎, <strong><?php echo $_SESSION['username']; ?></strong></div>
+ 			</div>			
+			
+			<div id="tab_list" class="ah-tab-wrapper">
+                <div class="ah-tab">
+                    <a class="ah-tab-item" data-ah-tab-active="true" href="">上載宣傳片</a>
+                    <a class="ah-tab-item" href="">檢視用戶數據 </a>
+                    <a class="ah-tab-item" href="">購買宣傳套餐</a> 
+	                <a class="ah-tab-item" href="">付款歷史</a> 
+					<a class="ah-tab-item" href="">顧客兌換商品 </a> 
+				    <a class="ah-tab-item" href="">增加顧客兌換商品</a> 
+				
+                </div>
+            </div>		
+			
+			
+			
+			
+			
+			
+   		   <div id="tab_list_content" class="ah-tab-content-wrapper">	
+			<div class="ah-tab-content" data-ah-tab-active="true" id="advvideo">
+			<form method="POST" action="adv_user_home.php" id="uploadVideoForm" name="uploadVideoForm" enctype="multipart/form-data">
+			<?php include('errors.php'); ?>
+			<div class="form-row">
+			     <div class="row row-space"><strong>上載宣傳片</strong>(只支援 mp4, mov 及 avi 影片)</div>				 
+ 			</div>			
+			<div class="form-row">
+			<?php 
+			$videouser = $_SESSION['username'];
+			$query = "SELECT approved FROM  advs_video WHERE username = '$videouser' and approved = 'T'";
+			$resultsofyou = mysqli_query($db, $query); 			
+			if (mysqli_num_rows($resultsofyou) == 1) {
+			?>
+			
+				<div class="row row-space"><strong>你的影片已被批准及上架</strong></div>				 
+				
+			<?php
+			}
+			?>
+			
+			<?php 
+			$videouser = $_SESSION['username'];
+			$query = "SELECT approved FROM  advs_video WHERE username = '$videouser' and approved = 'F'";
+			$resultsofyou = mysqli_query($db, $query); 			
+			if (mysqli_num_rows($resultsofyou) == 1) {
+			?>
+			
+				<div class="row row-space"><strong>你的影片不被批准 </strong></div>				 
+				
+			<?php
+			}
+			?>
+			
+			<?php 
+			$videouser = $_SESSION['username'];
+			$query = "SELECT approved FROM  advs_video WHERE username = '$videouser' and approved = 'P'";
+			$resultsofyou = mysqli_query($db, $query); 			
+			if (mysqli_num_rows($resultsofyou) == 1) {
+			?>
+			
+				<div class="row row-space"><strong>你的影片審核中</strong></div>				 
+				
+			<?php
+			}
+			?>
+			
+			
+			</div>			
+			
+			<div class="form-row">			
+			<div class="row row-space">                  
+			  <strong><input type="file" name="myvideo"/></strong></div> 				  
+            </div>			    						
+			
+			<button class="btn btn--radius-2 btn--red" name="upload_video" id="upload_video" type="submit">上載影片</button>
+			
+			<br/>
+			<input type='hidden' name='username' id='username' value='<?php echo $_SESSION['username']; ?>' />
+			<br/>
+			<br/>
+			</form>
+			</div>
+			
+			<div class="ah-tab-content" id="customeraccess" >
+			 			<?php 
+			$videouser = $_SESSION['username'];
+			$query = "SELECT cc.title as title, cc.age as age,ca.tm as tm  FROM  customer_access ca, customers cc   WHERE ca.advid = '$videouser' and ca.customerid = cc.username" ;
+			$results = mysqli_query($db, $query); 			
+			if (mysqli_num_rows($results) >= 1) {
+			?>
+			 
+ 			
+			<table border="1">
+			<thead>
+				<tr>
+					<th>稱呼</th>
+					<th>年齡</th>					
+					<th>掃瞄廣告時間</th>			
+				</tr>
+			</thead>
+	
+			<?php while ($row = mysqli_fetch_array($results)) { ?>
+				<tr>
+					<td><?php echo $row['title']; ?></td>					
+					<td><?php echo $row['age']; ?></td>	
+					<td><?php echo $row['tm']; ?></td>						
+			</tr>
+			<?php } ?>
+			</table>
+			 
+			<?php
+			}
+			?>
+			 
+	
+			 
+			</div>
+			<div class="ah-tab-content" id="customerpayment">
+<!--<iframe src="paypal.html?username=<?php echo $_SESSION['username']; ?>" width="100%" height="600" frameborder="0" scrolling="yes"></iframe>
+			 -->
+			 <object data="paypal.html?username=<?php echo $_SESSION['username']; ?>" width="100%" height="600" frameborder="0" scrolling="yes">
+					<embed src="paypal.html?username=<?php echo $_SESSION['username']; ?>" width="100%" height="600" frameborder="0" scrolling="yes"> </embed>
+					Error: Embedded data could not be displayed.
+			 </object>
+			 
+			 
+			 
+			</div>
+
+			<div class="ah-tab-content" id="paymenthistory" >
+			<?php 
+			$videouser = $_SESSION['username'];
+			$query = "SELECT * FROM  customer_payment  WHERE customerid = '$videouser'" ;
+			$results = mysqli_query($db, $query); 			
+			if (mysqli_num_rows($results) >= 1) {
+			?>
+			 
+ 			
+			<table border="1">
+			<thead>
+				<tr>
+					<th>日期</th>
+					<th>套餐種類</th>
+					<th>價錢</th>
+					<th>已付款</th>					
+					<th>已審核</th>			
+				</tr>
+			</thead>
+	
+			<?php while ($row = mysqli_fetch_array($results)) { ?>
+				<tr>
+					<td><?php echo $row['tm']; ?></td>					
+					<td><?php echo $row['item']; ?></td>					
+					<td><?php echo $row['price']; ?></td>					
+					<td><?php echo $row['paid']; ?></td>	
+					<td><?php echo $row['verified']; ?></td>						
+			</tr>
+			<?php } ?>
+			</table>
+			 
+			<?php
+			}
+			?>
+			 
+			</div>
+			
+			
+			
+			<div class="ah-tab-content" id="itemshop" >
+			<?php 
+			$videouser = $_SESSION['username'];
+			$query = "SELECT * FROM  item_shop  WHERE adv_id = '$videouser'" ;
+			$results = mysqli_query($db, $query); 			
+			if (mysqli_num_rows($results) >= 1) {
+			?>
+			 
+ 			
+			<table border="1">
+			<thead>
+				<tr>
+					<th>貨品編號</th>
+					<th>貨品名稱</th>
+					<th>貨品描述</th>
+					<th>貨品兌換分數</th>								
+					<th>貨品上架狀態</th>					
+					<th>貨品數目</th>					
+					<th>貨品圖像</th>								
+					<th>更新時間</th>
+					<th>行動</th>
+				</tr>
+			</thead>
+	
+			<?php while ($row = mysqli_fetch_array($results)) { ?>
+				<tr>
+					<td><?php echo $row['item_id']; ?></td>					
+					<td><?php echo $row['item_name']; ?></td>					
+					<td><?php echo $row['item_description']; ?></td>					
+					<td><?php echo $row['item_price']; ?></td>										
+					
+					<td><?php if ($row['item_status'] == 'N')
+							  {
+							  	echo "Approved";
+							  } else if ($row['item_status'] == 'P'){								  
+								 echo "Pending"; 
+							  } else if ($row['item_status'] == 'F'){
+								 echo "Rejected"; 
+							  }								  
+						?>
+					</td>
+					<td><?php echo $row['item_quantity']; ?></td>						
+					<td><img width="100" height="100" src="http://157.230.145.40/ops/pic/<?php echo $row['item_photo_path']; ?>"   ></td>											
+					<td><?php echo $row['tm']; ?></td>											
+					<td>
+						<a href="aserver.php?item_id=<?php echo $row['item_id']; ?>&delete_item=delete_item&adminuser=<?php echo $_SESSION['username']?>" class="edit_btn" >刪除</a>
+					</td>					
+			</tr>
+					
+			</tr>
+			<?php } ?>
+			</table>
+			 
+			<?php
+			}
+			?>
+			
+			</div>
+						
+
+			
+			
+		 <!--<div id="tab_list_content" class="ah-tab-content-wrapper">	-->
+			<div class="ah-tab-content" id="additem">
+			<form method="POST" action="adv_user_home.php" id="uploadItemForm" name="uploadItemForm" enctype="multipart/form-data">
+			<?php include('errors.php'); ?>
+			<div class="form-row">
+			     <div class="row row-space"><strong>上戴兌換貨品 </strong></div>				 
+ 			</div>			
+			<div >
+			<div class="form-row">			
+			  <div class="row row-space">                  
+			  <strong><input type="file" name="myphoto"/></strong>
+			  </div> 				  
+            </div>			    			
+
+			<p></p>
+
+			
+			<div class="form-row">
+              <div class="row row-space"><strong>貨品名稱:</strong> </div>
+                <div class="value">
+                  <div class="input-group">
+                     <input class="input--style-5" name="item_name" id='item_name' value="<?php echo $item_name; ?>" > <label for="item_name" class="error"></label>
+                  </div>
+               </div>
+            </div>
+			
+
+			<div class="form-row">
+              <div class="row row-space"><strong>貨品數目:</strong> </div>
+                <div class="value">
+				
+				
+                  <div class="input-group">
+				  
+				  
+				  
+	   			      <input class="input--style-5"   type="number" name="item_quantity"  id='item_quantity'  min="1" max="100" value="<?php echo $item_quantity; ?>" >    
+					  
+					  
+                     <label for="item_quantity" class="error"></label>
+                  </div>
+				  
+				  
+				  
+               </div>
+            </div>
+
+			
+			<div class="form-row">
+              <div class="row row-space"><strong>貨品描述:</strong> </div>
+                <div class="value">
+                  <div class="input-group">
+                     <input class="input--style-5" name="item_description" id='item_description' value="<?php echo $item_description; ?>" > <label for="item_description" class="error"></label>
+                  </div>
+               </div>
+            </div>
+			
+			<div class="form-row">
+              <div class="row row-space"><strong>貨品兌換分數:</strong> </div>
+                <div class="value">
+                  <div class="input-group">
+                     <input class="input--style-5" name="item_price" id='item_price' value="<?php echo $item_price; ?>" > <label for="item_price" class="error"></label>
+                  </div>
+               </div>
+            </div>
+			
+			<p></p>			
+			
+			
+			<button class="btn btn--radius-2 btn--red" name="upload_item" id="upload_item" type="submit">上載貨品</button>
+			
+			<br/>
+			<input type='hidden' name='username' id='username' value='<?php echo $_SESSION['username']; ?>' />
+			<br/>
+			<br/>
+			</form>
+			</div>
+				
+			</div>
+			
+			
+			
+			
+			<div class="form-row">
+				<div class="name"> <a href="adv_user_home.php?logout='1'" style="color: red;">登出</a></div>
+			</div>			
+			
+			
+			<?php endif ?>
+			
+            </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Jquery JS-->
+    <!--<script src="vendor/jquery/jquery.min.js"></script>-->
+    <!-- Vendor JS-->
+    <script src="vendor/select2/select2.min.js"></script>
+    <script src="vendor/datepicker/moment.min.js"></script>
+    <script src="vendor/datepicker/daterangepicker.js"></script>
+
+    <!-- Main JS-->
+    <script src="js/global.js"></script>
+
+
+</body><!-- This templates was made by Colorlib (https://colorlib.com) -->
+
+</html>
+<!-- end document-->
