2597f632e53cd94f78b909b3c34f4dde988650b6
comprobot <comprobot0503@gmail.com>
2019-09-05 20:28:02 +0800

    Update videoquery.php

--- videoquery.php
+++ videoquery.php
@@ -16,7 +16,7 @@ $result = $db->query($sql);
 if ($result->num_rows > 0) {
     // output data of each row
     while($row = $result->fetch_assoc()) {
-        echo $row["filename"].','. $row["qrcode"]." \n ";
+        echo $row["filename"].','. $row["username"]." \n ";
     }
 } else {
     echo "0 results";
