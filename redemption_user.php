7bb7475eefbefd80fbffe623ea8ce182f53aa533
comprobot <comprobot0503@gmail.com>
2019-09-28 12:01:58 +0800

    Update redemption_user.php

--- redemption_user.php
+++ redemption_user.php
@@ -20,6 +20,28 @@
         <link rel="stylesheet" href="style.css"/>
         <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
         <link href="https://fonts.googleapis.com/css?family=Noto+Sans+TC&display=swap" rel="stylesheet">        
+	<script>
+		var wConfirm = window.confirm;  
+		window.confirm = function (message) {  
+    		try {  
+        		var iframe = document.createElement("IFRAME");  
+        		iframe.style.display = "none";  
+        		iframe.setAttribute("src", 'data:text/plain,');  
+        		document.documentElement.appendChild(iframe);  
+        		var alertFrame = window.frames[0];  
+        		var iwindow = alertFrame.window;  
+        		if (iwindow == undefined) {  
+            			iwindow = alertFrame.contentWindow;  
+        		}  
+        		var result=iwindow.confirm(message);  
+        		iframe.parentNode.removeChild(iframe);  
+        			return result;
+    			}  
+    			catch (exc) {  
+        			return wConfirm(message);  
+    			}  
+		} 
+	</script>
     </head>
     <body>
        <div id="header">
