48ed7c23705bf3cfbacf825cfcefa55de743dfc0
comprobot <comprobot0503@gmail.com>
2019-10-14 15:17:13 +0800

    Update genEcoupon1.php

--- genEcoupon1.php
+++ genEcoupon1.php
@@ -10,6 +10,11 @@ if (isset($_GET['redeem_code'])) {
 
 $target_dir = dirname(__FILE__) ."/pic/";
 $target_file = $target_dir . "tempcode.png";
+	
+$target_dir_1 = dirname(__FILE__) ."/pic/";
+$target_file_1 = $target_dir . "E.jpg";
+	
+	
 
 $value = 'http://157.230.145.40/ops/redemption_claim.php?redeem_code='.$_GET['redeem_code']; //二维码内容   
 $errorCorrectionLevel = 'L';//容错级别   
@@ -28,7 +33,10 @@ $pdf->SetFont('Arial','B',16);
 $pdf->Cell(40,30,'Redeem code='.$_GET['redeem_code']);
 
 
-$pdf->Image($target_file,40,40,100,100);
+$pdf->Image($target_file_1,0,0,200,300);
+	
+	
+	
 
 /*
 $pdf->SetFont('Arial','B',16);
