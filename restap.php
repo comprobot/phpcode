cd63cb3132205d9d9fa8f8cf6d10915f403f98d5
comprobot <comprobot0503@gmail.com>
2019-10-14 19:48:34 +0800

    Update restap.php

--- restap.php
+++ restap.php
@@ -201,7 +201,7 @@ if (isset($_GET['reg_phone'])) {
   // receive all input values from the form
   $username = mysqli_real_escape_string($db, $_GET['username']);    
   $phone = mysqli_real_escape_string($db, $_GET['phone']);  
-  $area_code = mysqli_real_escape_string($db, $_GET['phone']);  
+  $area_code = mysqli_real_escape_string($db, $_GET['area_phone']);  
   
 
   // form validation: ensure that the form is correctly filled ...
