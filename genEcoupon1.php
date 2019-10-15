0d3e96f7c3d7a0a850361412037d963d4652768d
comprobot <comprobot0503@gmail.com>
2019-10-15 12:20:38 +0800

    Update genEcoupon1.php

--- genEcoupon1.php
+++ genEcoupon1.php
@@ -8,6 +8,9 @@ if (isset($_GET['redeem_code'])) {
   if (empty($_GET['redeem_code'])) {
   	header('location: user_login.php');
   }
+  
+$redeem_code = $_GET['redeem_code'];
+
 
 $target_dir = dirname(__FILE__) ."/pic/";
 $target_file = $target_dir . "tempcode.png";
@@ -23,6 +26,10 @@ $matrixPointSize = 6;//生成图片大小
 //生成二维码图片   
 QRcode::png($value, $target_file, $errorCorrectionLevel, $matrixPointSize, 2); 
 
+$user_check_query = "select * from customer_item c , item_shop i where c.item_id = i.item_id and c.item_redeem_code = '$redeem_code'";
+$result = mysqli_query($db, $user_check_query);
+$user = mysqli_fetch_assoc($result);
+  	
 
 $pdf = new FPDF();
 $pdf->AddPage();
@@ -40,30 +47,29 @@ $pdf->Image($target_file_1,0,0,210,297);
 
 $target_file_text = $target_dir . "tempcodefont.png";
 
-$font = "m.ttf";
+$font = "./m.ttf";
 
 
-//$text = "中國心明天心";
+$text = $user['item_shop_name'];
+$text1 = $user['item_name'];
 
-$text = iconv("GB2312", "UTF-8", "回忆经典");
-$text = "Chinese heart";
 
 $my_img = imagecreate( 320, 80 );                             //width & height
 $background  = imagecolorallocate( $my_img, 255,   255,   255 );
 $text_colour = imagecolorallocate( $my_img, 0, 0, 255 );
-
-
-//$white = imagecolorallocate($my_img, 255, 255, 255);
-
 $black = imagecolorallocate($my_img, 0, 0, 0);
-
-
-imagettftext($im, 20, 0, 0, 0, $black, $font, $text);
+imagettftext($im, 24, 0, 0, 30, $black, $font, $text);
+imagettftext($im, 24, 0, 0, 60, $black, $font, $text1);
 imagepng( $my_img,$target_file_text );
 
 
 
+
+
+
 $pdf->Image($target_file,142,118,52,52);
+
+
 
 $pdf->Image($target_file_text,15,60,80,20);
 
