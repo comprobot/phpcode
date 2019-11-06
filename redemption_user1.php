967f31766335146ff5c7c7fe5139f778c681173b
comprobot <comprobot0503@gmail.com>
2019-11-06 10:33:27 +0800

    Update and rename redemption_user.php to redemption_user1.php

--- redemption_user1.php
+++ redemption_user1.php
@@ -21,47 +21,7 @@
         <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
         <link href="https://fonts.googleapis.com/css?family=Noto+Sans+TC&display=swap" rel="stylesheet">
 <script>
-    <!--
-var wAlert = window.alert;
-window.alert = function (message) {
-    try {
-        var iframe = document.createElement("IFRAME");
-        iframe.style.display = "none";
-        iframe.setAttribute("src", 'data:text/plain,');
-        document.documentElement.appendChild(iframe);
-        var alertFrame = window.frames[0];
-        var iwindow = alertFrame.window;
-        if (iwindow == undefined) {
-            iwindow = alertFrame.contentWindow;
-        }
-        iwindow.alert(message);
-        iframe.parentNode.removeChild(iframe);
-    }
-    catch (exc) {
-        return wAlert(message);
-    }
-}
-var wConfirm = window.confirm;
-window.confirm = function (message) {
-    try {
-        var iframe = document.createElement("IFRAME");
-        iframe.style.display = "none";
-        iframe.setAttribute("src", 'data:text/plain,');
-        document.documentElement.appendChild(iframe);
-        var alertFrame = window.frames[0];
-        var iwindow = alertFrame.window;
-        if (iwindow == undefined) {
-            iwindow = alertFrame.contentWindow;
-        }
-        var result=iwindow.confirm(message);
-        iframe.parentNode.removeChild(iframe);
-        return result;
-    }
-    catch (exc) {
-        return wConfirm(message);
-    }
-} 
-    -->
+    
 </script>
     </head>
     <body>
